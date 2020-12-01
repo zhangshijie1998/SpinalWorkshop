cf54a2d6c99f817812aee07c9375bdffad8af12f
Jacob <69901447+zhangshijie1998@users.noreply.github.com>
2020-12-01 18:35:01 +0800

    Update Counter.scala

--- src/main/scala/workshop/counter/Counter.scala
+++ src/main/scala/workshop/counter/Counter.scala
@@ -4,10 +4,18 @@ import spinal.core._
 
 case class Counter(width: Int) extends Component {
   val io = new Bundle {
-    val clear    = in  Bool
-    val value    = out UInt(width bits)
-    val full     = out Bool
+    val clear = in Bool
+    val value = out UInt (width bits)
+    val full = out Bool
   }
 
-  //TODO define the logic
+  val counter = Reg(UInt(width bits)) init (0)
+  counter := counter + 1
+  when(io.clear) {
+    counter := 0
+  }
+
+  io.value := counter
+  io.full := counter === U(counter.range -> true)
+
 }
